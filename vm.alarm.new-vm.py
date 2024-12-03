6b141f93d394c65b67af667a234b48462fd51107
KnutssonDevelopment <KnutssonDevelopment@users.noreply.github.com>
2024-12-03 12:22:38 +0100

    Update vm.alarm.new-vm.py

--- vm.alarm.new-vm.py
+++ vm.alarm.new-vm.py
@@ -6,10 +6,19 @@ import socket
 
 from pathlib import Path
 import pytz
-from datetime import datetime
+from datetime import datetime, timedelta
+
+sys.path.extend(os.environ['VMWARE_PYTHON_PATH'].split(';'))
+script_path=Path(os.path.abspath(__file__)).parent
+sys.path.append(script_path)
+
+from pyVim import connect
+from pyVim.connect import SmartConnect
+from pyVmomi import vim
 
 local_tz = pytz.timezone('Europe/Copenhagen')
 now = datetime.now(local_tz)
+task_msg = "Task: Import OVF package"
 
 # Custom Variables
 creatorCustomAttribute = 'CreatedBy'
@@ -22,18 +31,9 @@ else:
     username = "alarm_user@vsphere.local"
     password = "PASSWORD"
 
-sys.path.extend(os.environ['VMWARE_PYTHON_PATH'].split(';'))
-script_path=Path(os.path.abspath(__file__)).parent
-sys.path.append(script_path)
-
-from pyVim import connect
-from pyVim.connect import SmartConnect
-from pyVmomi import vim
-
-
 def get_vcenter_connection(hostname=None, username=None, password=None):
-
-    s=ssl.SSLContext(ssl.PROTOCOL_SSLv23) # For VC 6.5/6.0 s=ssl.SSLContext(ssl.PROTOCOL_TLSv1)
+    s=ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
+    s.check_hostname = False
     s.verify_mode=ssl.CERT_NONE
 
     try:
@@ -61,6 +61,28 @@ def custom_attribute_exists(content, field_name):
     return False
 
 
+def get_vm_events(si: vim.ServiceInstance, vm: vim.VirtualMachine, max_events=1000):
+    by_entity = vim.event.EventFilterSpec.ByEntity(entity=vm, recursion="self")
+    filter_spec = vim.event.EventFilterSpec(entity=by_entity, maxCount=max_events)
+
+    content=si.content
+    event_manager = content.eventManager
+    events = event_manager.QueryEvent(filter_spec)
+
+    return events
+
+
+def get_all_task_events_by_compute_resource(si: vim.ServiceInstance, start: datetime, end: datetime, compute_resource):
+    events_by_time = vim.event.EventFilterSpec.ByTime(beginTime=start, endTime=end)
+    events_by_entity = vim.event.EventFilterSpec.ByEntity(entity=compute_resource.computeResource, recursion='self')
+    filter_spec = vim.event.EventFilterSpec(time=events_by_time, entity=events_by_entity, eventTypeId=['vim.event.TaskEvent'])
+    content=si.content
+    event_manager = content.eventManager
+    events = event_manager.QueryEvent(filter_spec)
+
+    return events
+
+
 # Create a new custom attribute
 def create_custom_attribute(content, field_name, mo_type=vim.VirtualMachine):
     if not custom_attribute_exists(content, field_name):
@@ -80,23 +102,66 @@ def main():
         print('export VMWARE_ALARM_TARGET_NAME="TestVM"')
         print('export VMWARE_ALARM_TARGET_ID="vm-123456"')
         print('export VMWARE_ALARM_EVENT_USERNAME="DOMAIN\\User"')
-
+        
         exit(1)
 
     hostname = socket.gethostname()
 
+    # Connecting to vCenter
     vc_connection = get_vcenter_connection(hostname, username, password)
     if (not vc_connection):
         print(f"ERROR(2): Could not connect to vCenter: {hostname}")
         exit(2)
 
+    # Getting VirtualMachine
     content=vc_connection.content
-
     vm = find_vm_by_id(content, alarm_target_id, alarm_target_name)
 
     if (not vm):
         print(f"ERROR(3): Could not find vm with name: '{alarm_target_name}' and id: '{alarm_target_id}'")
         exit(3)
+
+
+    if "vpxd-extension" in alarm_user:
+        # Trying to get the right user account from the events on the VirtualMachine
+        events = get_vm_events(si=vc_connection, vm=vm, max_events=1)
+
+        if len(events) == 0:
+            print(f"WARNING(1): Did not find any event for VirtualMachine: {alarm_target_name}")
+            alarm_user = "USER Unknown"
+        else:
+            # Get the first event ever created on this VM
+            vm_created_time = events[0].createdTime
+            compute_resource  = events[0].computeResource
+
+            possible_events = []
+            search_interval = 1
+            overlap_interval = 1
+            while len(possible_events) == 0:
+                start = vm_created_time - timedelta(minutes=search_interval)
+                end = vm_created_time - timedelta(minutes=(search_interval - overlap_interval))
+                global_events = get_all_task_events_by_compute_resource(si=vc_connection, start=start, end=end, compute_resource=compute_resource)
+
+                for event in global_events:
+                    if event.fullFormattedMessage == task_msg:
+                        if event.computeResource.computeResource == compute_resource.computeResource:
+                            print(f"{event.createdTime} {event.userName} {event.fullFormattedMessage}")
+                            possible_events.append(event)
+
+                if len(possible_events) > 0:
+                    # We found an event
+                    break
+                elif search_interval > 240:
+                    # We were unable to find the event within 4 hours of the deployment
+                    break
+                else:
+                    # Keep looking
+                    search_interval += 1
+
+            if len(possible_events) > 0:
+                alarm_user = possible_events[0].userName
+            else:
+                alarm_user = "USER Unknown"
 
     try:
         create_custom_attribute(content, creatorCustomAttribute)
